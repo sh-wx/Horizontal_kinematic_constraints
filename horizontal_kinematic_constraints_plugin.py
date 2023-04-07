c5eaf4b4212481e24e2af9d3d43e0a80744485ae
sh-wx <130127239+sh-wx@users.noreply.github.com>
2023-04-07 16:50:46 +0800

    Add files via upload

---
+++ horizontal_kinematic_constraints_plugin.py
@@ -0,0 +1,19 @@
+from abaqusGui import getAFXApp, Activator, AFXMode
+from abaqusConstants import ALL
+import os
+thisPath = os.path.abspath(__file__)
+thisDir = os.path.dirname(thisPath)
+
+toolset = getAFXApp().getAFXMainWindow().getPluginToolset()
+toolset.registerGuiMenuButton(
+    buttonText='Horizontal_kinematic_constraints', 
+    object=Activator(os.path.join(thisDir, 'horizontal_kinematic_constraintsDB.py')),
+    kernelInitString='import Horizontal_kinematic_constraints',
+    messageId=AFXMode.ID_ACTIVATE,
+    icon=None,
+    applicableModules=ALL,
+    version='N/A',
+    author='N/A',
+    description='N/A',
+    helpUrl='N/A'
+)
