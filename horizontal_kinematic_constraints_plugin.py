476ac87e40980b1aa6c3e97ef8f2d2d560c7719c
sh-wx <130127239+sh-wx@users.noreply.github.com>
2023-05-20 11:09:29 +0800

    Add files via upload

--- horizontal_kinematic_constraints_plugin.py
+++ horizontal_kinematic_constraints_plugin.py
@@ -1,16 +1,71 @@
-from abaqusGui import getAFXApp, Activator, AFXMode
+from abaqusGui import *
 from abaqusConstants import ALL
-import os
+import osutils, os
+
+
+###########################################################################
+# Class definition
+###########################################################################
+
+class Horizontal_kinematic_constraints_plugin(AFXForm):
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def __init__(self, owner):
+        
+        # Construct the base class.
+        #
+        AFXForm.__init__(self, owner)
+        self.radioButtonGroups = {}
+
+        self.cmd = AFXGuiCommand(mode=self, method='main',
+            objectName='Horizontal_kinematic_constraints', registerQuery=False)
+        pickedDefault = ''
+        self.ModelnameKw = AFXStringKeyword(self.cmd, 'Modelname', True)
+        self.PartNameKw = AFXStringKeyword(self.cmd, 'PartName', True)
+        self.instancenameKw = AFXStringKeyword(self.cmd, 'instancename', True, 'Part-1-1')
+        self.verticaldirectionKw = AFXStringKeyword(self.cmd, 'verticaldirection', True, 'Y')
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def getFirstDialog(self):
+
+        import horizontal_kinematic_constraintsDB
+        return horizontal_kinematic_constraintsDB.Horizontal_kinematic_constraintsDB(self)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def doCustomChecks(self):
+
+        # Try to set the appropriate radio button on. If the user did
+        # not specify any buttons to be on, do nothing.
+        #
+        for kw1,kw2,d in self.radioButtonGroups.values():
+            try:
+                value = d[ kw1.getValue() ]
+                kw2.setValue(value)
+            except:
+                pass
+        return True
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def okToCancel(self):
+
+        # No need to close the dialog when a file operation (such
+        # as New or Open) or model change is executed.
+        #
+        return False
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Register the plug-in
+#
 thisPath = os.path.abspath(__file__)
 thisDir = os.path.dirname(thisPath)
 
 toolset = getAFXApp().getAFXMainWindow().getPluginToolset()
 toolset.registerGuiMenuButton(
     buttonText='Horizontal_kinematic_constraints', 
-    object=Activator(os.path.join(thisDir, 'horizontal_kinematic_constraintsDB.py')),
-    kernelInitString='import Horizontal_kinematic_constraints',
+    object=Horizontal_kinematic_constraints_plugin(toolset),
     messageId=AFXMode.ID_ACTIVATE,
     icon=None,
+    kernelInitString='import Horizontal_kinematic_constraints',
     applicableModules=ALL,
     version='N/A',
     author='N/A',
